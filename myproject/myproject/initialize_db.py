dd16dbab7476443a7d152519f391324e45940a7e
spencerhcheng <spencerhcheng@gmail.com>
2018-04-18 02:42:08 -0700

    Add import of Base from Models

--- myproject/myproject/initialize_db.py
+++ myproject/myproject/initialize_db.py
@@ -15,13 +15,11 @@ from .models import (
     Base,
     )
 
-
 def usage(argv):
     cmd = os.path.basename(argv[0])
     print('usage: %s <config_uri>\n'
           '(example: "%s development.ini")' % (cmd, cmd))
     sys.exit(1)
-
 
 def main(argv=sys.argv):
     if len(argv) != 2:
