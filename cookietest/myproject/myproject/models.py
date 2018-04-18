f3705275efe4c10f16eaa31230443e3fa74e6209
spencerhcheng <spencerhcheng@gmail.com>
2018-04-17 18:48:01 -0700

    Add instantiation of Page class

--- cookietest/myproject/myproject/models.py
+++ cookietest/myproject/myproject/models.py
@@ -1,9 +1,11 @@
 from pyramid.security import Allow, Everyone
+from datetime import datetime
 
 from sqlalchemy import (
     Column,
     Integer,
     Text,
+    DateTime,
     )
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -21,11 +23,21 @@ Base = declarative_base()
 
 
 class Page(Base):
-    __tablename__ = 'wikipages'
+    """
+    Attributes and functions for Page class
+    """
+    __tablename__ = 'access'
     uid = Column(Integer, primary_key=True)
-    title = Column(Text, unique=True)
-    body = Column(Text)
+    session_identifier = Column(Text)
+    accessed_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    page = Column(Text)
 
+    def __init__(self, session_identifier, page):
+        """
+        Instantiation of new Page class
+        """
+        self.session_identifier = session_identifier
+        self.page = page
 
 class Root(object):
     __acl__ = [(Allow, Everyone, 'view'),
